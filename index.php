4f17473196a59d2054da6d20a39af9e8ff875018
Eddie <kb36600@gmail.com>
2020-09-21 16:18:30 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+    body {
+        font-family:微軟正黑體;
+    }
+</style>
+<title>楊翔鴻 0921</title>
+</head>
+<body>
+<h2>楊翔鴻 0921 git 練習記錄</h2>
+<hr>
+<?php include "menu.php"; ?>
+<hr>
+<?php include "footer.php"; ?>
+</body>
+</html>
